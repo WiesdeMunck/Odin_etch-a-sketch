3377af0f3920e45861be697a8e36b1b68491abf4
WiesdeMunck <wies-wies@hotmail.com>
2023-03-10 15:26:45 +0100

    grid layout and grid selectors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,40 @@
 // main container
-let mainContainer = document.getElementById('container');
+const mainContainer = document.getElementById('container');
+
+// amount of grids
+mainContainer.style.display = "grid";
+mainContainer.style.gridTemplate = "50px / repeat(20, 50px)";
+
 
 // main container styling
+mainContainer.style.backgroundColor = "#666666";
+mainContainer.style.width = "500px";
+mainContainer.style.gridAutoRows = "50px";
+// mainContainer.style.height = "500px";
 
-// Create divs
-function createGrid() {
-for (let i = 0; i <= 16; i++) {
+
+// Create div's
+function createGrid(amountDivs) {
+for (let i = 0; i <= amountDivs; i++) {
     let createDiv = document.createElement('div');
     mainContainer.append(createDiv)
+
+    mainContainer.children[i].style.backgroundColor = "#00ff00"
+    
 }
 
 }
-createGrid()
+
+createGrid(399);
+
+let child = mainContainer.firstElementChild;
+
+child.style.backgroundColor = "#0000ff"
+
+
+
+
+
 
 
 
