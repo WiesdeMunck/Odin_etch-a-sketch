6d673ff69a1ec33da60cef55255f29a66b992e76
WiesdeMunck <wies-wies@hotmail.com>
2023-03-10 14:13:27 +0100

    File structure and first call to make divs

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+// main container
+let mainContainer = document.getElementById('container');
+
+// main container styling
+
+// Create divs
+function createGrid() {
+for (let i = 0; i <= 16; i++) {
+    let createDiv = document.createElement('div');
+    mainContainer.append(createDiv)
+}
+
+}
+createGrid()
+
+
+
+
+
+
+
