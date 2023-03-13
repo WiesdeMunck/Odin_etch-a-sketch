256b43d12a015518963e6f77e29921a6202cf90a
WiesdeMunck <wies-wies@hotmail.com>
2023-03-13 10:32:33 +0100

    Functions for creating the grid items and drawing

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 // main container
-const mainContainer = document.getElementById('container');
+const mainContainer = document.querySelector('.container');
 
 // amount of grids
 mainContainer.style.display = "grid";
@@ -8,37 +8,26 @@ mainContainer.style.gridTemplate = "50px / repeat(20, 50px)";
 
 // main container styling
 mainContainer.style.backgroundColor = "#666666";
-mainContainer.style.width = "500px";
 mainContainer.style.gridAutoRows = "50px";
-// mainContainer.style.height = "500px";
-
 
 // Create div's
 function createGrid(amountDivs) {
 for (let i = 0; i <= amountDivs; i++) {
     let createDiv = document.createElement('div');
     mainContainer.append(createDiv)
-
-    mainContainer.children[i].style.backgroundColor = "#00ff00"
-    
+    mainContainer.children[i].classList.add("hello")
 }
 
 }
 
+// Creates grid items
 createGrid(399);
 
-let child = mainContainer.firstElementChild;
+// 
 
-child.style.backgroundColor = "#0000ff"
-
-
-
-
-
-
-
-
-
-
-
-
+// Used event  to target children of the grid
+mainContainer.addEventListener('mouseover', function (e) {
+    if (e.target.matches('.hello')) {
+      e.target.classList.add('active');
+    }
+  });
