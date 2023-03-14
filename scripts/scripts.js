71d0cf5ff2fd28e8b54547509de0388dbcea8af0
WiesdeMunck <wies-wies@hotmail.com>
2023-03-14 09:33:02 +0100

    Drawing and reset function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,17 +2,20 @@
 const mainContainer = document.querySelector('.container');
 
 // amount of grids
+let amountOfGrids = 400
+
 mainContainer.style.display = "grid";
-mainContainer.style.gridTemplate = "50px / repeat(20, 50px)";
+mainContainer.style.gridTemplate = "30px / repeat(20, 30px)";
+mainContainer.style.gridTemplate = "30px / repeat(20, 30px)";
 
 
 // main container styling
 mainContainer.style.backgroundColor = "#666666";
-mainContainer.style.gridAutoRows = "50px";
+mainContainer.style.gridAutoRows = "30px";
 
 // Create div's
 function createGrid(amountDivs) {
-for (let i = 0; i <= amountDivs; i++) {
+for (let i = 0; i < amountDivs; i++) {
     let createDiv = document.createElement('div');
     mainContainer.append(createDiv)
     mainContainer.children[i].classList.add("hello")
@@ -21,13 +24,32 @@ for (let i = 0; i <= amountDivs; i++) {
 }
 
 // Creates grid items
-createGrid(399);
-
+createGrid(amountOfGrids);
 // 
 
+
+
 // Used event  to target children of the grid
-mainContainer.addEventListener('mouseover', function (e) {
+mainContainer.addEventListener('mousedown', function (e) {
     if (e.target.matches('.hello')) {
-      e.target.classList.add('active');
+      e.target.classList.toggle('active');
     }
   });
+
+
+//   reset 
+
+const resetBtn = document.getElementById('resetBtn')
+
+resetBtn.addEventListener('mousedown', function() {
+
+    while (mainContainer.firstChild) {
+        mainContainer.removeChild(mainContainer.lastChild)
+    }
+    createGrid(amountOfGrids);
+})
+
+
+
+
+
